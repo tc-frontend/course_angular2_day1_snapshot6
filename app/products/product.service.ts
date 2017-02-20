f65a9a32b960f43a487557ed33afa4ace5149a44
tc-frontend <roberto.loreto@avanade.com>
2017-02-20 18:23:27 +0100

    +Add HttpModle to app-module ~Replace the getProducts method by http.get and rxjs.map methos ~The getProducts return a Observable<IProduct[]>

--- app/products/product.service.ts
+++ app/products/product.service.ts
@@ -1,30 +1,18 @@
-import {Injectable} from '@angular/core'
-import { IProduct} from './product'
+import { Injectable } from '@angular/core'
+import { IProduct } from './product'
+import { Http, Response } from '@angular/http'
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/operator/map'
 @Injectable()
 export class ProductService{
+    private _productUrl = 'api/products/products.json'
 
-    getProducts(): IProduct[] {
-        return [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-        }
-    ];
+    constructor(private _http: Http){
+
+    }
+
+    getProducts(): Observable<IProduct[]> {
+        return this._http.get(this._productUrl)
+            .map((response: Response)=> <IProduct[]>response.json());
     }
 }
