3fce211e83ddbee0f20d323c90ca448156704a1e
tc-frontend <roberto.loreto@avanade.com>
2017-02-20 18:13:37 +0100

    +Use the getProducts methos of the service to populate de array

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,47 +1,30 @@
 import { Component, OnInit } from '@angular/core'
 import { IProduct} from './product'
+import { ProductService } from './product.service'
 
 @Component({
     selector: 'pm-products',
     moduleId: module.id,
     templateUrl: 'product-list.component.html',
     styleUrls: [ 'product-list.component.css']
-})
+}) 
 export class ProductListComponent implements OnInit   {
     pageTitle: string = 'Product List';
     imageWith: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = 'cart';
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-        }
-    ];
+    products: IProduct[] ;
+    constructor(private _productService: ProductService){
+       
+    }
 
-    toggleImage(): void {
+    toggleImage(): void { 
         this.showImage = !this.showImage;
     }
 
     ngOnInit(): void {
-        console.log('OnInit')
+         this.products = this._productService.getProducts();
     }
 
     onRatingCicked(message: string):void{
